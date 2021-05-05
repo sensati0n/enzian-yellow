c48983e6ee99df09867c6442b6bae2b7b818733b
phischolz <scholzens28@gmail.com>
2021-05-05 11:53:37 +0200

    optimized parameter omission logic

--- src/ethereum/deploy-enzian.prod.js
+++ src/ethereum/deploy-enzian.prod.js
@@ -7,21 +7,19 @@ const basicEnzianCompiled = require('./build/BasicEnzian.json');
  */
 const deployContractAndLibrary = async(web3Wrapper, compiled, privateKey) => {
 
+  let opts = { };
+  if(!privateKey){
+    opts = { from: web3Wrapper.accounts[0]  }
+  }
+  
   let basicEnzian_deployed;
-  if(privateKey) {
-    if(compiled) {
-      basicEnzian_deployed = await web3Wrapper.deployContractByAbiAndBytecode(compiled.abi, compiled.evm.bytecode.object, {   }, privateKey);
-    } else {
-      basicEnzian_deployed = await web3Wrapper.deployContractByAbiAndBytecode(basicEnzianCompiled.abi, basicEnzianCompiled.evm.bytecode.object, {   }, privateKey);
-    }
+  if(compiled) {
+    basicEnzian_deployed = await web3Wrapper.deployContractByAbiAndBytecode(compiled.abi, compiled.evm.bytecode.object, opts, privateKey);
+  } else {
+    basicEnzian_deployed = await web3Wrapper.deployContractByAbiAndBytecode(basicEnzianCompiled.abi, basicEnzianCompiled.evm.bytecode.object, opts, privateKey);
   }
-  else {
-    if(compiled) {
-      basicEnzian_deployed = await web3Wrapper.deployContractByAbiAndBytecode(compiled.abi, compiled.evm.bytecode.object, { from: web3Wrapper.accounts[0]  }, privateKey);
-    } else {
-      basicEnzian_deployed = await web3Wrapper.deployContractByAbiAndBytecode(basicEnzianCompiled.abi, basicEnzianCompiled.evm.bytecode.object, { from: web3Wrapper.accounts[0]  }, privateKey);
-    }
-  }
+
+
   
 
 
